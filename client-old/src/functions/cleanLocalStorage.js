1ab931fa0d5cbda85fd76aaa88138be7db725f87
Israelramirez9 <israelramirez.am@gmail.com>
2023-09-04 17:16:54 -0300

    deploying next frontend

--- client-old/src/functions/cleanLocalStorage.js
+++
@@ -1,6 +0,0 @@
-import { removeAccessToken, removeRefreshToken } from "../helpers/token";
-
-export function cleanLocalStorage() {
-    removeAccessToken();
-    removeRefreshToken();
-}
--- client-old/src/services/session.services.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import { API_URL } from "../config/api";
-
-export const startSession = async (user) => {
-    const session = await axios.post(API_URL + "/session", user)
-    return session
-}
