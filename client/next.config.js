48a84b70fea334148b427ad65f22f35416d36974
Israelramirez9 <israelramirez.am@gmail.com>
2023-09-05 17:50:43 -0300

    added customized commands  to compile to github

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,10 @@
 /**  @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  distDir: process.env.NODE_ENV === 'development' ? undefined : '../docs',
-  output: process.env.NODE_ENV === 'development' ? undefined : 'export',
   images: {
     unoptimized: true
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_URL || '/login-trello'
+  basePath: process.env.NEXT_PUBLIC_BASE_URL
 }
 
 module.exports = nextConfig
