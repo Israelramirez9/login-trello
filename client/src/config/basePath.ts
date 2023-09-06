c7fd332d6ec93d20238f1b0db79054f7238b3664
Israelramirez9 <israelramirez.am@gmail.com>
2023-09-06 11:33:40 -0300

    empty commit

--- client/src/config/basePath.ts
+++ client/src/config/basePath.ts
@@ -1,1 +1,1 @@
-export const BASE_PATH = process.env.NODE_ENV === 'development' ? '' : process.env.NEXT_PUBLIC_BASE_URL || '/login-trello'
+export const BASE_PATH = process.env.NEXT_PUBLIC_BASE_URL || '/login-trello'
